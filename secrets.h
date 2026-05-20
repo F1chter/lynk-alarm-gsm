8291a1827884dc4c0bf44fe9a9194b06ce617429
F1chter <fichteralex@gmail.com>
2026-05-20 23:04:41 +0300

    fake secrets

--- secrets.h
+++ secrets.h
@@ -1,1 +1,6 @@
 const String ALLOW_INCOME_NUMBERS = "+380501234567+380991234567"; 
+
+const String WIFI_SSID = "SSID";
+const String WIFI_PASS = "PASSWORD";
+const String BOT_TOKEN = "111:AAEx6";
+const String CHAT_ID = "111111111";
